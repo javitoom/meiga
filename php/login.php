e20dd813b849d46dcd41cc07c7ae470470e428a2
Javi <javitoom96@gmail.com>
2016-08-28 13:57:41 +0200

    meta responsive

--- php/login.php
+++ php/login.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION["formularioLog"])) {
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/cabecera.css">
     <link rel="stylesheet" href="../css/login.css">
     <title>Document</title>
