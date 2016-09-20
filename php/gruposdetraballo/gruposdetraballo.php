edd826fb93a267a8bcd5a9389b86a069438cfca8
Javi <javitoom96@gmail.com>
2016-09-20 16:32:09 +0200

    he mejorado grupostraballo

--- php/gruposdetraballo/gruposdetraballo.php
+++ php/gruposdetraballo/gruposdetraballo.php
@@ -11,16 +11,16 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/cabecera.css">
-    <link rel="stylesheet" href="../css/login.css">
-    <link rel="stylesheet" href="../css/w3.css">
+    <link rel="stylesheet" href="../../css/cabecera.css">
+    <link rel="stylesheet" href="../../css/login.css">
+    <link rel="stylesheet" href="../../css/w3.css">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
     <title>Document</title>
 </head>
 <body>
 <?php
-include_once("cabecera.php");
-include_once("menu.php");
+include_once("../cabecera.php");
+include_once("../menu.php");
 ?>
 <div class="w3-display-container" style="height: 500px">
     <div class="w3-display-middle">
@@ -30,6 +30,6 @@ include_once("menu.php");
         <h3>Seguiremos actualizando a páxina con máis información ata o final do mes.</br>Faite socio <a href=#>aqui</a>.</h3>
     </div>
 </div>
-<script src="../js/navbar.js"></script>
+<script src="../../js/navbar.js"></script>
 </body>
 </html>
