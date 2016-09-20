5e1498b2c69a463cf145c875326bb6e44552f714
Quique Mangut Gutiérrez <quique mangut gutirrez>
2016-09-20 16:45:34 +0200

    arreglados enlaces 2

---
+++ php/Intercambios/Intercambios.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+/**
+ * Created by PhpStorm.
+ * User: quiqu
+ * Date: 07/08/2016
+ * Time: 20:53
+ */?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/cabecera.css">
+    <link rel="stylesheet" href="../../css/login.css">
+    <link rel="stylesheet" href="../../css/w3.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+include_once("../cabecera.php");
+include_once("../menu.php");
+?>
+<div class="w3-display-container" style="height: 500px">
+    <div class="w3-display-middle">
+        <h1>Aquí poderás descubrir todo sobre a túa asociación, dende a historia, os membros, as actividades, os intercambios que realizamos e moito máis!</h1>
+        <h2>Un saúdo meigoso.</h2>
+        <!--        TODO-->
+        <h3>Seguiremos actualizando a páxina con máis información ata o final do mes.<br>Faite socio <a href=#>aqui</a>.</h3>
+    </div>
+</div>
+<script src="../../js/navbar.js"></script>
+</body>
+</html>
--- php/menu.php
+++ php/menu.php
@@ -15,46 +15,46 @@
         <li class="w3-hide-small w3-dropdown-hover">
             <a href="#" class="w3-hover-none w3-padding-large">Quenes somos?</a>
             <div class="w3-dropdown-content w3-white w3-card-4">
-                <a href="#">Coñécenos!</a>
-                <a href="#">Consulta o Reglamento (ROI)</a>
+                <a href="/php/Quenessomos/Coñecenos.php">Coñécenos!</a>
+                <a href="/php/Quenessomos/consultaoroi.php">Consulta o Reglamento (ROI)</a>
             </div>
         </li>
         <li class="w3-hide-small w3-dropdown-hover">
             <!--En href es donde hay que poner los enlaces TODO-->
             <a href="/php/gruposdetraballo/gruposdetraballo.php" class="w3-hover-none w3-padding-large">Grupos de traballo</a>
             <div class="w3-dropdown-content w3-white w3-card-4">
-                <a href="#">Saúde Pública</a>
-                <a href="#">Educación Médica</a>
-                <a href="#">Dereitos humanos e paz</a>
-                <a href="#">Salud reproductiva, sexualidade e sida</a>
-                <a href="#">Presidencia e Vicepresidencia</a>
-                <a href="#">Secretaría e Tesouraría</a>
-                <a href="#">Webmáster</a>
+                <a href="/php/gruposdetraballo/saudepublica.php">Saúde Pública</a>
+                <a href="/php/gruposdetraballo/educacionmedica.php">Educación Médica</a>
+                <a href="/php/gruposdetraballo/dereitoshumanosepaz.php">Dereitos humanos e paz</a>
+                <a href="/php/gruposdetraballo/saludreproductiva.php">Salud reproductiva, sexualidade e sida</a>
+                <a href="/php/gruposdetraballo/Presidenciaevicepresidencia.php">Presidencia e Vicepresidencia</a>
+                <a href="/php/gruposdetraballo/Secretariaetesoureria.php">Secretaría e Tesouraría</a>
+                <a href="/php/gruposdetraballo/Webmaster.php">Webmáster</a>
             </div>
         </li>
-        <li class="w3-hide-small"><a class="w3-padding-large" href="#home">Actividades</a></li>
+        <li class="w3-hide-small"><a class="w3-padding-large" href="/php/Actividades/Actividades.php">Actividades</a></li>
         <li class="w3-hide-small w3-dropdown-hover">
-            <a href="#" class="w3-hover-none w3-padding-large">Intercambios</a>
+            <a href="/php/Intercambios/Intercambios.php" class="w3-hover-none w3-padding-large">Intercambios</a>
             <div class="w3-dropdown-content w3-white w3-card-4">
-                <a href="#">Coñece o noso equipo</a>
-                <a href="#">Intercambios do A ao Z</a>
-                <a href="#">Queres ser Persoa de Contacto?</a>
-                <a href="#">Acolle a un estudante de intercambio</a>
-                <a href="#">Eles estiveron en Santiago!</a>
-                <a href="#">Eles foron de intercambio!</a>
+                <a href="/php/Intercambios/Coñeceonosoequipo.php">Coñece o noso equipo</a>
+                <a href="/php/Intercambios/IntercambiosdoAaoZ.php">Intercambios do A ao Z</a>
+                <a href="/php/Intercambios/Queresserpessoadecontacto.php">Queres ser Persoa de Contacto?</a>
+                <a href="/php/Intercambios/acolleaunestudante.php">Acolle a un estudante de intercambio</a>
+                <a href="/php/Intercambios/ElesestiveronenSantiago.php">Eles estiveron en Santiago!</a>
+                <a href="/php/Intercambios/Elesforondeintercambio.php">Eles foron de intercambio!</a>
             </div>
         </li>
         <li class="w3-hide-small w3-dropdown-hover">
-            <a href="#" class="w3-hover-none w3-padding-large">Socios</a>
+            <a href="/php/Socios/Socios.php" class="w3-hover-none w3-padding-large">Socios</a>
             <div class="w3-dropdown-content w3-white w3-card-4">
-                <a href="#">Faite socio!</a>
-                <a href="#">Queres gañar puntos?</a>
-                <a href="#">FAQ</a>
-                <a href="#">Participa nas reunións</a>
-                <a href="#">"O oso dos ósos"</a>
+                <a href="/php/Socios/Faitesocio.php">Faite socio!</a>
+                <a href="/php/Socios/queresgañarpuntos.php">Queres gañar puntos?</a>
+                <a href="/php/Socios/FAQ.php">FAQ</a>
+                <a href="/php/Socios/participanasreunions.php">Participa nas reunións</a>
+                <a href="/php/Socios/osodososos.php">"O oso dos ósos"</a>
             </div>
         </li>
-        <li class="w3-hide-small"><a class="w3-padding-large" href="#home">Contacto</a></li>
+        <li class="w3-hide-small"><a class="w3-padding-large" href="/php/Contacto/Contacto.php">Contacto</a></li>
         <?php if(!isset($_SESSION["nomUsuario"])){  ?>
         <li id="registrate" class="w3-hide-small"><a class="w3-padding-large" href="registrate.php">Registrate</a></li>
         <li id="registrate" class="w3-hide-small"><a class="w3-padding-large"  href="login.php">Login</a></li>
@@ -68,10 +68,10 @@
 <div id="navDemo" class="w3-hide w3-hide-large w3-hide-medium" >
     <ul class="w3-navbar w3-left-align w3-black">
         <li class=" w3-dropdown-hover" style="background-color: #000; color: rgb(204, 204, 204)">
-            <a href="#" class="w3-hover-none w3-padding-large">Quenes somos?</a>
+            <a href="/php/Quenessomos/Quenessomos.php" class="w3-hover-none w3-padding-large">Quenes somos?</a>
             <div class="w3-dropdown-content w3-white w3-card-4">
-                <a href="#">Coñécenos!</a>
-                <a href="#">Consulta o Reglamento (ROI)</a>
+                <a href="/php/Quenessomos/Coñecenos.php">Coñécenos!</a>
+                <a href="/php/Quenessomos/consultaoroi.php">Consulta o Reglamento (ROI)</a>
             </div>
         </li>
         <li class=" w3-dropdown-hover">
@@ -110,11 +110,11 @@
         </li>
         <li class=""><a class="w3-padding-large" href="#home">Contacto</a></li>
         <?php if(!isset($_SESSION["nomUsuario"])){  ?>
-            <li class=""><a class="w3-padding-large" href="registrate.php">Registrate</a></li>
-            <li class=""><a class="w3-padding-large"  href="login.php">Login</a></li>
+            <li class=""><a class="w3-padding-large" href="/php/registrate.php">Registrate</a></li>
+            <li class=""><a class="w3-padding-large"  href="/php/login.php">Login</a></li>
         <?php }else{?>
-            <li class=""><a class="w3-padding-large" href="logout.php">Logout</a></li>
-            <li class=""><a class="w3-padding-large" href="login.php">Perfil</a></li>
+            <li class=""><a class="w3-padding-large" href="/php/logout.php">Logout</a></li>
+            <li class=""><a class="w3-padding-large" href="/php/login.php">Perfil</a></li>
         <?php } ?>
     </ul>
 </div>
