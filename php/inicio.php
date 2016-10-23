253c04435f437d666e95349396468ef0f4bb1c0d
javi <javitoom96@gmail.com>
2016-10-23 21:05:52 +0200

    Texto inicio nuevo y cambio de directorio

--- php/inicio.php
+++ php/inicio.php
@@ -22,8 +22,8 @@ session_start();
  include_once("cabecera.php");
 include_once("menu.php");
 ?>
-<div class="w3-display-container" style="height: 500px">
-    <div class="w3-display-middle">
+<div >
+    <div>
         <h1>Aquí poderás descubrir todo sobre a túa asociación, dende a historia, os membros, as actividades, os intercambios que realizamos e moito máis!</h1>
         <h2>Un saúdo meigoso.</h2>
 <!--        TODO-->
